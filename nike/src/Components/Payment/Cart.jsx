26eeef6fe3700ee0d8358f75b025d347fff01020
yogesh <bhardwajy621@gmail.com>
2023-09-30 11:23:15 +0530

    rectify the rozaypay amound

--- nike/src/Components/Payment/Cart.jsx
+++ nike/src/Components/Payment/Cart.jsx
@@ -36,7 +36,7 @@ export const CartPage = () => {
   const handlePayment = async () => {
     const options = {
       key: "rzp_test_N7JAloi8EO00qI",
-      amount: totalPrice * 100 + diliveryCharges,
+      amount: totalPrice * 100 + diliveryCharges * 100,
       currency: "INR",
       name,
       description: "Payment for your service",
